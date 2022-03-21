6e4164fbe6f23b7d81053ca88d13dfe0ea05f65e
Kauan <kauan-20@hotmail.com>
2022-03-21 21:13:23 +0100

    adc tipo booleano

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// Boolean
+// true -> verdadeiro  // false -> falso
+
+const primNum = 5;
+const segNum = 10;
+
+ 
+
+console.log(primNum === segNum);
+
+const texto1 = "alura"
+const texto2 = "Alura"
+
+console.log(texto1 === texto2)
