278730b3dce4ed5bee18cd90ee5bb268bbcf81de
Kauan <kauan-20@hotmail.com>
2022-03-21 21:05:20 +0100

    adc exemplos

--- type-string.js
+++ type-string.js
@@ -1,6 +1,6 @@
 const texto1 = "Ola, Mundo!";
 const texto2 = 'Olá,Mundo!';
-const senha = "senhaMaluca123456!";
+const senha2 = "senhaMaluca123456!";
 const StringNumeros = "354781";
 
 const citacao = "Meu nome é ";
@@ -9,6 +9,17 @@ const meuNome = "Leonardo"
 //concatenação (+)
 
 console.log(citacao + meuNome) 
+
+//casos Maiusculo Minusculo - length tamanho
+
+const input = "belo horizonte"
+const cidade = "Belo Horizonte"
+const cidadeMinusculo = cidade.toLowerCase();
+
+console.log(input === cidadeMinusculo);
+
+const senha = "minhaSenha123"
+console.log(senha.length)
 
 // template string OU template literal
 
