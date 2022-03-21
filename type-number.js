836ba26481b8df0444a0f32fc296e4e5113986fe
Kauan <kauan-20@hotmail.com>
2022-03-21 20:46:10 +0100

    type number adicionado a pasta

---
+++ type-number.js
@@ -0,0 +1,23 @@
+//tipo number
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+//Nan -> Not a Number (não é um número)
+
+const alura = "Alura"
+
+console.log(alura * primeiroNumero)
