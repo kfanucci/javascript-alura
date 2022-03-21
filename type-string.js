6fbe0737cf00f8a75184424b4a213710c9ade5a6
Kauan <kauan-20@hotmail.com>
2022-03-21 20:56:32 +0100

    adc string ao curso de js

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Ola, Mundo!";
+const texto2 = 'Olá,Mundo!';
+const senha = "senhaMaluca123456!";
+const StringNumeros = "354781";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo"
+
+//concatenação (+)
+
+console.log(citacao + meuNome) 
+
+// template string OU template literal
+
